b8baa2c5dab110fae3be71947abf61db316faeac
itsmarkodowd <godfather_mark@hotmail.com>
2019-08-11 01:33:21 +0100

    extra cleanup

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -45,7 +45,7 @@ class SignIn extends React.Component {
 				      <div className="mt3">
 				        <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
 				        <input 
-				        	className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+				        	className="white pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
 				        	type="email" 
 				        	name="email-address"  
 				        	id="email-address" 
@@ -56,7 +56,7 @@ class SignIn extends React.Component {
 				      <div className="mv3">
 				        <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
 				        <input 
-				        	className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+				        	className="white pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
 				        	type="password" 
 				        	name="password"  
 									id="password"
